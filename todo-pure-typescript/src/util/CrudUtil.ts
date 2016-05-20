9246b6f51936787e3644b79664352da5178215e3
reginaldocmrg <reginaldocmrg@hotmail.com>
2016-05-19 22:12:53 -0300

    Started trying to solve promise is not defined
    I tryied without success resolve the promise problem and i get stuck even with simple angular typings configuration.

--- todo-pure-typescript/src/util/CrudUtil.ts
+++ todo-pure-typescript/src/util/CrudUtil.ts
@@ -1,3 +1,7 @@
 export class CrudUtil {
-    
+    public save(entity : Entity) : Promise<any>;
+    public retrieve() : Promise<any>;
+    public retrieveList() : Promise<any>;
+    public delete(entity : Entity) : Promise<any>;
+    public create(entity : Entity) : Promise<any>;
 }

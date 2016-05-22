e0325615f35cf4647ca87c5b9c2cee76177be455
Maike Mota <maike_henrique@hotmail.com>
2016-05-21 21:01:07 -0300

    New Property : doneDate
    New property to show when a Todo was done

--- todo-angular2/app/src/core/model/Todo.ts
+++ todo-angular2/app/src/core/model/Todo.ts
@@ -1,8 +1,10 @@
 import { BaseModel } from  './BaseModel';
 
-export class Todo extends BaseModel{
-    
-    public name : string;
-    public description : string;
+export class Todo extends BaseModel {
+
+    public name: string;
+    public description: string;
     public done: boolean;
+    public doneDate: Date;
+    
 }

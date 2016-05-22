b238d4c6d80079ca4a9459790ff1143f04796a9b
Maike Mota <maike_henrique@hotmail.com>
2016-05-21 22:53:12 -0300

    Added MomentoJS and Materialize to tsd.

--- todo-angular2/typings/material/material.d.ts
+++ todo-angular2/typings/material/material.d.ts
@@ -3,3 +3,7 @@ interface JQuery {
     leanModal();
     leanModal(configuration: Object);
 }
+
+declare class Materialize {
+    static showStaggeredList(selector: string);
+}
--- todo-angular2/typings/tsd.d.ts
+++ todo-angular2/typings/tsd.d.ts
@@ -1,2 +1,4 @@
 /// <reference path="jquery/jquery.d.ts" />
 /// <reference path="material/material.d.ts" />
+/// <reference path="moment/moment-node.d.ts" />
+/// <reference path="moment/moment.d.ts" />

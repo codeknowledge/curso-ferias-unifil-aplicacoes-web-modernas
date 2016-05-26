523b4e60692a868b6cd698569b75a81f47089822
Maike Mota <maike_henrique@hotmail.com>
2016-05-26 08:10:34 -0300

    Replace mocked list with controller response

--- todo-angular2/core/component/todolist/TodoListComponent.ts
+++ todo-angular2/core/component/todolist/TodoListComponent.ts
@@ -1,43 +1,68 @@
-import { Component } from '@angular/core';
+//Angular2
+import { Component, provide } from '@angular/core';
 
+//Model
 import { Todo } from '../../model/Todo';
+
+//Component
 import { TodoComponent } from '../todo/TodoComponent';
 import { TodoCreation } from '../todocreation/TodoCreation';
+
+//Controller
+import { TodoController } from '../../controller/TodoController';
+
+//Service
+import { LocalStorageCrudService } from '../../service/LocalStorageCrudService';
+
+//Util
+import { ApplicationConfig } from '../../util/ApplicationConfig';
 
 @Component({
   selector: 'todo-list',
   templateUrl: 'core/component/todolist/TodoListComponent.html',
   styleUrls: ['core/component/todolist/TodoListComponent.css'],
-  directives: [TodoComponent, TodoCreation]
+  directives: [TodoComponent, TodoCreation],
+  providers: [
+    provide(ApplicationConfig.CRUD_SERVICE_TOKEN, { useClass: LocalStorageCrudService }),
+    provide(TodoController, { useClass: TodoController })
+  ]
 })
 export class TodoListComponent {
 
-  private todos = new Array<Todo>();
+  private todos: Array<Todo> = new Array<Todo>();
 
   constructor(
+    private controller: TodoController
   ) {
 
   }
 
   ngAfterViewInit(): void {
 
-
-    for (let i = 0; i < 10; i++) {
-      let todo = new Todo();
-      todo.id = `${i}`;
-      todo.creationDate = new Date();
-      todo.name = `Todo ${i + 1}`;
-      todo.description = `Description ${i + 1}`;
-      todo.done = i % 2 ? true : false;
-      if (todo.done) {
-        todo.doneDate = new Date(1463796582);
+    this.controller.retrieveList().then((todos: Array<Todo>) => {
+      todos.sort((a: Todo, b: Todo) => {
+        return a.order - b.order;
+      });
+      this.todos = todos;
+      if (this.hasTodos) {
+        setTimeout(() => {
+          Materialize.showStaggeredList('todo');
+        });
       }
-      this.todos.push(todo);
-    }
-    setTimeout(() => {
-      Materialize.showStaggeredList('todo');
+    }).catch((error) => {
+      //TODO: TRATAR ERRO
     });
 
+  }
+
+  private get hasTodos(): boolean {
+    return this.todos.length > 0;
+  }
+
+  private onTodoStatusChange(todo: Todo) {
+    this.controller.save(todo).catch((error) => {
+      //TODO: TRATAR ERRO
+    });
   }
 
 }

bfdada3ef4c45e6c0b2bc442205c7378c7bf3900
Maike Mota <maike_henrique@hotmail.com>
2016-05-26 08:09:28 -0300

    Implementation of controller to work with Todo

--- todo-angular2/core/controller/TodoController.ts
+++ todo-angular2/core/controller/TodoController.ts
@@ -1,8 +1,51 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
 
 import { Todo } from '../model/Todo';
 import { AbstractController } from './AbstractController';
 
-export abstract class TodoController extends AbstractController<Todo>{
+@Injectable()
+export class TodoController extends AbstractController<Todo>{
+    /**
+     *
+     */
+    constructor(injector: Injector) {
+        super(injector);
+    }
+
+
+
+    public beforeCreate(): void {
+
+    }
+
+    public afterCreate(): void {
+
+    }
+
+    public beforeSave(): void {
+
+    }
+    
+    public afterSave(): void {
+
+    }
+
+    public retrieve(id: string): Promise<Todo> {
+        return null;
+    }
+
+    public retrieveList(): Promise<Array<Todo>> {
+        return this.crudService.retrieveList();
+    }
+    public beforeDelete(): void {
+
+    }
+
+    public delete(entity: Todo): Promise<boolean> {
+        return null;
+
+    }
+
+    public afterDelete(): void { }
 
 }

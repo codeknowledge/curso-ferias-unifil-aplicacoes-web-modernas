501f68af7953ca9ef5017c7d827acc322ed6c2c4
Maike Mota <maike_henrique@hotmail.com>
2016-05-26 08:09:04 -0300

    Defines default behavior for Controllers

--- todo-angular2/core/controller/AbstractController.ts
+++ todo-angular2/core/controller/AbstractController.ts
@@ -1,11 +1,57 @@
+//Angular2
+import { Injector } from '@angular/core';
+
+//Model
 import { Entity } from '../model/Entity';
+
+//Service
+import { AbstractCrudService } from '../service/AbstractCrudService';
+
+//util
+import { ApplicationConfig } from '../util/ApplicationConfig';
 
 export abstract class AbstractController<T extends Entity> {
 
-    public abstract save(entity: T): Promise<T>;
+    protected crudService: AbstractCrudService<T>;
+
+    constructor(private injector: Injector) {
+        this.crudService = injector.get(ApplicationConfig.CRUD_SERVICE_TOKEN);
+    }
+
+    public abstract beforeCreate(): void;
+
+    public create(entity: T): Promise<T> {
+        return new Promise<T>((resolve, reject) => {
+            this.beforeCreate();
+            this.crudService.create(entity).then((entity: T) => {
+                resolve(entity);
+                this.afterCreate();
+            }).catch((error) => {
+                reject(error);
+            });
+        });
+    }
+    public abstract afterCreate(): void;
+
+    public abstract beforeSave(): void;
+
+    public save(entity: T): Promise<T> {
+        return new Promise<T>((resolve, reject) => {
+            this.beforeSave();
+            this.crudService.save(entity).then((entity: T) => {
+                resolve(entity);
+                this.afterSave();
+            }).catch((error) => {
+                reject(error);
+            });
+        });
+    }
+    public abstract afterSave(): void;
+
     public abstract retrieve(id: string): Promise<T>;
     public abstract retrieveList(): Promise<any>;
+    public abstract beforeDelete(): void;
     public abstract delete(entity: T): Promise<boolean>;
-    public abstract create(entity: T): Promise<T>;
+    public abstract afterDelete(): void;
 
 }

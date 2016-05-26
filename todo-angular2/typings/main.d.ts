e40e38ba9e76860f51666ac248cbaaaf536ba036
Maike Mota <maike_henrique@hotmail.com>
2016-05-26 08:06:10 -0300

    Replace ambient imports

--- todo-angular2/typings/main.d.ts
+++ todo-angular2/typings/main.d.ts
@@ -1,5 +1,3 @@
-/// <reference path="main/ambient/es6-shim/index.d.ts" />
-/// <reference path="main/ambient/jasmine/index.d.ts" />
-/// <reference path="main/ambient/node/index.d.ts" />
 /// <reference path="tsd.d.ts" />
+/// <reference path="browser.d.ts" />
 

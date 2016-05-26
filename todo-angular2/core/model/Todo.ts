8a428803ad132197ee6bd1162f6bb04b00b36eaf
Maike Mota <maike_henrique@hotmail.com>
2016-05-26 08:09:52 -0300

    Initialize done property as false

--- todo-angular2/core/model/Todo.ts
+++ todo-angular2/core/model/Todo.ts
@@ -5,7 +5,7 @@ export class Todo extends Entity {
     public name: string;
     public order: number;
     public description: string;
-    public done: boolean;
+    public done: boolean = false;;
     public doneDate: Date;
 
 }

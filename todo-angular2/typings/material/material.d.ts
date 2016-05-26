22cf68254eabb8a648957091714443c444dab687
Maike Mota <maike_henrique@hotmail.com>
2016-05-26 08:05:30 -0300

    Added Missing methods;

--- todo-angular2/typings/material/material.d.ts
+++ todo-angular2/typings/material/material.d.ts
@@ -2,6 +2,8 @@ interface JQuery {
     characterCounter();
     leanModal();
     leanModal(configuration: Object);
+    closeModal();
+    dropdown(configuration: Object);
 }
 
 declare class Materialize {

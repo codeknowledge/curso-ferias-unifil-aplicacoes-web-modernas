ed5604d93594f8678cf054082422749c6929a4b5
Maike Mota <maike_henrique@hotmail.com>
2016-05-26 08:08:36 -0300

    Implements a Crud to Work with LocalStorage

---
+++ todo-angular2/core/service/LocalStorageCrudService.ts
@@ -0,0 +1,61 @@
+//Model
+import { Entity } from '../model/Entity';
+
+//Service
+import { AbstractCrudService } from './AbstractCrudService';
+
+//Util
+import { UUID } from '../util/UUID';
+
+export class LocalStorageCrudService<T extends Entity> extends AbstractCrudService<T> {
+
+    public create(entity: T): Promise<T> {
+        return new Promise<T>((resolve, reject) => {
+            entity.id = UUID.generateUIID();
+            entity.creationDate = new Date();
+            this.saveToStorage(entity);
+            resolve(entity);
+        });
+    }
+
+    public save(entity: T): Promise<T> {
+        return new Promise<T>((resolve, reject) => {
+            this.saveToStorage(entity);
+            resolve(entity);
+        });
+    }
+
+    public retrieve(id: string): Promise<T> {
+        return new Promise<T>((resolve, reject) => {
+            resolve(this.retrieveFromStorage(id));
+        });
+    }
+
+    public retrieveList(): Promise<Array<T>> {
+        return new Promise<Array<T>>((resolve, reject) => {
+            let list: Array<T> = new Array<T>();
+            for (let i = 0, len = localStorage.length; i < len; ++i) {
+                let key: string = localStorage.key(i);
+                list.push(this.retrieveFromStorage(key));
+            }
+            resolve(list);
+        });
+
+    }
+
+    public delete(entity: T): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            localStorage.removeItem(entity.id);
+            resolve();
+        });
+    }
+
+    private retrieveFromStorage(id: string): T {
+        let entity: T = JSON.parse(localStorage.getItem(id));
+        return entity;
+    }
+
+    private saveToStorage(entity: T) {
+        localStorage.setItem(entity.id, JSON.stringify(entity));
+    }
+}

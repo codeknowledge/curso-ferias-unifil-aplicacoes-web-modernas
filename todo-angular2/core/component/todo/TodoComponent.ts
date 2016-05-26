9cc611cafda00cb954de1c6058a932dd8c858b76
Maike Mota <maike_henrique@hotmail.com>
2016-05-26 08:11:50 -0300

    TodoComponent now emits a event when todo status change.

--- todo-angular2/core/component/todo/TodoComponent.ts
+++ todo-angular2/core/component/todo/TodoComponent.ts
@@ -1,7 +1,15 @@
-import { Component, Input } from '@angular/core';
+//Angular2
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
+//Model
 import { Todo } from '../../model/Todo';
-import { RelativeTimePipe } from '../../pipe/RelativeTimePipe'
+
+//Util
+import { UUID } from '../../util/UUID';
+
+
+//Pipe
+import { RelativeTimePipe } from '../../pipe/RelativeTimePipe';
 
 @Component({
     selector: 'todo',
@@ -12,12 +20,15 @@ import { RelativeTimePipe } from '../../pipe/RelativeTimePipe'
 })
 export class TodoComponent {
 
+    private id: string = UUID.s4();
+
     @Input() todo: Todo;
+    @Output() statusChange: EventEmitter<Todo> = new EventEmitter();
 
     private toggleState(): void {
         this.todo.done = !this.todo.done;
         this.setDoneDate();
-
+        this.statusChange.emit(this.todo);
     }
 
     private setDoneDate(): void {
@@ -26,5 +37,17 @@ export class TodoComponent {
         } else {
             this.todo.doneDate = undefined;
         }
+    }
+
+    ngAfterViewInit(): void {
+        jQuery('#more' + this.id).dropdown({
+            inDuration: 300,
+            outDuration: 225,
+            constrain_width: false, // Does not change width of dropdown to that of the activator
+            hover: true, // Activate on hover
+            gutter: 0, // Spacing from edge
+            belowOrigin: false, // Displays dropdown below the button
+            alignment: 'left' // Displays dropdown with edge aligned to the left of button
+        });
     }
 }

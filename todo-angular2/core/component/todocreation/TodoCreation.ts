eedfcd09e5b1163e855c2c05e1c048150c6bd488
Maike Mota <maike_henrique@hotmail.com>
2016-05-26 08:11:19 -0300

    TodoCreation now use TodoController
    Visual Improvements.

--- todo-angular2/core/component/todocreation/TodoCreation.ts
+++ todo-angular2/core/component/todocreation/TodoCreation.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+//Angular2
+import { Component, Input } from '@angular/core';
+
+import { Todo } from '../../model/Todo';
+
+//Controller
+import { TodoController } from '../../controller/TodoController';
 
 @Component({
     selector: 'todo-creation',
@@ -7,14 +13,47 @@ import { Component } from '@angular/core';
 })
 export class TodoCreation {
 
+    @Input() todos: Array<Todo>;
+
+    private todo: Todo = new Todo();
+    private createMore: boolean = false;
+
+    /**
+     *
+     */
+    constructor(
+        private controller: TodoController
+    ) {
+
+    }
+
     ngAfterViewInit(): void {
         jQuery('.modal-trigger').leanModal({
             dismissible: true,
             in_duration: 300, // Transition in duration
             out_duration: 200, // Transition out duration
         });
-
         jQuery('input[type="text"], textarea').characterCounter();
+    }
+
+    private submit(): void {
+        this.controller.create(this.todo).then((todo: Todo) => {
+            this.todos.push(todo);
+            if (!this.createMore) {
+                this.closeModal();
+            }
+            this.resetForm();
+        }).catch((error) => {
+            console.debug(JSON.stringify(error));
+        });
+    }
+
+    private resetForm(): void {
+        this.todo = new Todo();
+    }
+
+    private closeModal(): void {
+        jQuery('.modal').closeModal();
     }
 
 }

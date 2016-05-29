52f18407761d5c7e5c54568d291931306f056663
Maike Mota <maike_henrique@hotmail.com>
2016-05-29 16:31:25 -0300

    Change Materialize Reference to Semantic reference.

--- todo-angular2/typings/tsd.d.ts
+++ todo-angular2/typings/tsd.d.ts
@@ -1,4 +1,4 @@
 /// <reference path="jquery/jquery.d.ts" />
-/// <reference path="material/material.d.ts" />
+/// <reference path="semantic/semantic.d.ts" />
 /// <reference path="moment/moment-node.d.ts" />
 /// <reference path="moment/moment.d.ts" />

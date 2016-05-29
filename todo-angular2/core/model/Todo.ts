a9138d15a73c24435e69181c4522b96fda32b7d7
Maike Mota <maike_henrique@hotmail.com>
2016-05-29 16:35:17 -0300

    Removed order property

--- todo-angular2/core/model/Todo.ts
+++ todo-angular2/core/model/Todo.ts
@@ -3,7 +3,6 @@ import { Entity } from  './Entity';
 export class Todo extends Entity {
 
     public name: string;
-    public order: number;
     public description: string;
     public done: boolean = false;;
     public doneDate: Date;

8dd9773667714fe3bc457e570f8ae0ea29751f96
Maike Mota <maike_henrique@hotmail.com>
2016-05-29 16:34:12 -0300

    Uses mini buttons to Edit and Delete Actions.

--- todo-angular2/core/component/todo/TodoComponent.ts
+++ todo-angular2/core/component/todo/TodoComponent.ts
@@ -40,16 +40,4 @@ export class TodoComponent {
             this.todo.doneDate = undefined;
         }
     }
-
-    ngAfterViewInit(): void {
-        jQuery('#more' + this.id).dropdown({
-            inDuration: 300,
-            outDuration: 225,
-            constrain_width: false, // Does not change width of dropdown to that of the activator
-            hover: true, // Activate on hover
-            gutter: 0, // Spacing from edge
-            belowOrigin: false, // Displays dropdown below the button
-            alignment: 'left' // Displays dropdown with edge aligned to the left of button
-        });
-    }
 }
